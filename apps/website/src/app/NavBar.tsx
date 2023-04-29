e26ddb35251091a74da16a604d1fb023e578a983
Grace Durant <gdurant34@gmail.com>
2023-04-28 18:45:45 -0700

    added my info to template

--- apps/website/src/app/NavBar.tsx
+++ apps/website/src/app/NavBar.tsx
@@ -1,109 +1,225 @@
-import Box from '@mui/material/Box'
-import Drawer from '@mui/material/Drawer'
-import CssBaseline from '@mui/material/CssBaseline'
-import AppBar from '@mui/material/AppBar'
-import Toolbar from '@mui/material/Toolbar'
-import List from '@mui/material/List'
-import Typography from '@mui/material/Typography'
-import Divider from '@mui/material/Divider'
-import ListItem from '@mui/material/ListItem'
-import ListItemButton from '@mui/material/ListItemButton'
-import ListItemIcon from '@mui/material/ListItemIcon'
-import ListItemText from '@mui/material/ListItemText'
-import InboxIcon from '@mui/icons-material/MoveToInbox'
-import MailIcon from '@mui/icons-material/Mail'
-
-const drawerWidth = 240
+import { Link } from 'react-router-dom';
 
 export default function NavBar() {
   return (
-    <Box sx={{display: 'flex'}}>
-      <CssBaseline />
-      <AppBar
-        position="fixed"
-        sx={{width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px`}}
-      >
-        <Toolbar>
-          <Typography variant="h6" noWrap component="div">
-            Permanent drawer
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
-            width: drawerWidth,
-            boxSizing: 'border-box',
-          },
-        }}
-        variant="permanent"
-        anchor="left"
-      >
-        <Toolbar />
-        <Divider />
-        <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
-      <Box
-        component="main"
-        sx={{flexGrow: 1, bgcolor: 'background.default', p: 3}}
-      >
-        <Toolbar />
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
-          dolor purus non enim praesent elementum facilisis leo vel. Risus at
-          ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
-          quisque non tellus. Convallis convallis tellus id interdum velit
-          laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
-          adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
-          integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
-          eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
-          quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
-          vivamus at augue. At augue eget arcu dictum varius duis at consectetur
-          lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
-          faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
-          ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
-          elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
-          sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
-          mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
-          risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
-          purus viverra accumsan in. In hendrerit gravida rutrum quisque non
-          tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
-          morbi tristique senectus et. Adipiscing elit duis tristique
-          sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
-      </Box>
-    </Box>
-  )
+    <aside className="ml-[-100%] fixed z-10 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen border-r bg-white transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]">
+      <div>
+        <div className="-mx-6 px-6 py-4">
+          <Link to="/" title="home">
+            {/* <img
+                src="https://tailus.io/sources/blocks/stats-cards/preview/images/logo.svg"
+                className="w-32"
+                alt="tailus logo"
+              /> */}
+          </Link>
+        </div>
+
+        <div className="mt-8 text-center">
+          <img
+              src="https://tailus.io/sources/blocks/stats-cards/preview/images/second_user.webp"
+              alt=""
+              className="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28"
+            />
+          <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">
+            Grace Durant
+          </h5>
+          <span className="hidden text-gray-400 lg:block">
+            Software Engineer
+          </span>
+        </div>
+
+        <ul className="space-y-2 tracking-wide mt-8">
+        <li>
+            <Link
+              to="/"
+              className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  className="fill-current text-gray-600 group-hover:text-cyan-600"
+                  fillRule="evenodd"
+                  d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z"
+                  clipRule="evenodd"
+                />
+                <path
+                  className="fill-current text-gray-300 group-hover:text-cyan-300"
+                  d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z"
+                />
+              </svg>
+              <span className="group-hover:text-gray-700">Home</span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/resume"
+              aria-label="resume"
+              className="relative px-4 py-3 flex items-center space-x-4 rounded-xl text-white bg-gradient-to-r from-sky-600 to-cyan-400"
+            >
+              <svg className="-ml-1 h-6 w-6" viewBox="0 0 24 24" fill="none">
+                <path
+                  d="M6 8a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2V8ZM6 15a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2v-1Z"
+                  className="fill-current text-cyan-400 dark:fill-slate-600"
+                ></path>
+                <path
+                  d="M13 8a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2V8Z"
+                  className="fill-current text-cyan-200 group-hover:text-cyan-300"
+                ></path>
+                <path
+                  d="M13 15a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-1Z"
+                  className="fill-current group-hover:text-sky-300"
+                ></path>
+              </svg>
+              <span className="-mr-1 font-medium">Resume</span>
+            </Link>
+          </li>
+          <li>
+            {/* <Link
+                to=""
+                className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group"
+                >
+                <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                >
+                <path
+                className="fill-current text-gray-300 group-hover:text-cyan-300"
+                fillRule="evenodd"
+                d="M2 6a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1H8a3 3 0 00-3 3v1.5a1.5 1.5 0 01-3 0V6z"
+                clipRule="evenodd"
+                />
+                <path
+                className="fill-current text-gray-600 group-hover:text-cyan-600"
+                d="M6 12a2 2 0 012-2h8a2 2 0 012 2v2a2 2 0 01-2 2H2h2a2 2 0 002-2v-2z"
+                />
+                </svg>
+                <span className="group-hover:text-gray-700">Categories</span>
+              </Link> */}
+          </li>
+          <li>
+            <Link
+              to="#"
+              className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  className="fill-current text-gray-600 group-hover:text-cyan-600"
+                  fillRule="evenodd"
+                  d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z"
+                  clipRule="evenodd"
+                />
+                <path
+                  className="fill-current text-gray-300 group-hover:text-cyan-300"
+                  d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z"
+                />
+              </svg>
+              <span className="group-hover:text-gray-700">About Me</span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="#"
+              className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  className="fill-current text-gray-600 group-hover:text-cyan-600"
+                  d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"
+                />
+                <path
+                  className="fill-current text-gray-300 group-hover:text-cyan-300"
+                  d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"
+                />
+              </svg>
+              <span className="group-hover:text-gray-700">Blog</span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="#"
+              className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  className="fill-current text-gray-300 group-hover:text-cyan-300"
+                  d="M4 4a2 2 0 00-2 2v1h16V6a2 2 0 00-2-2H4z"
+                />
+                <path
+                  className="fill-current text-gray-600 group-hover:text-cyan-600"
+                  fillRule="evenodd"
+                  d="M18 9H2v5a2 2 0 002 2h12a2 2 0 002-2V9zM4 13a1 1 0 011-1h1a1 1 0 110 2H5a1 1 0 01-1-1zm5-1a1 1 0 100 2h1a1 1 0 100-2H9z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              <span className="group-hover:text-gray-700">YouTube</span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="#"
+              className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  className="fill-current text-gray-600 group-hover:text-cyan-600"
+                  d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"
+                />
+                <path
+                  className="fill-current text-gray-300 group-hover:text-cyan-300"
+                  d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"
+                />
+              </svg>
+              <span className="group-hover:text-gray-700">Dancer</span>
+            </Link>
+          </li>
+        </ul>
+      </div>
+
+      <div className="px-6 -mx-6 pt-4 flex justify-between items-center border-t">
+        <button className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+            />
+          </svg>
+          <span className="group-hover:text-gray-700">Logout</span>
+        </button>
+      </div>
+    </aside>
+  );
 }
