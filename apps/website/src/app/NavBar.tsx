1a3207cc213872b39085d78cf6b99eaeb0ee55de
Grace Durant <gdurant34@gmail.com>
2023-05-08 14:38:00 -0700

    removed unnecessary code from navbar

--- apps/website/src/app/NavBar.tsx
+++ apps/website/src/app/NavBar.tsx
@@ -12,11 +12,6 @@ export default function NavBar() {
       <div>
         <div className="-mx-6 px-6 py-4">
           <Link to="/" title="home">
-            {/* <img
-                src="https://tailus.io/sources/blocks/stats-cards/preview/images/logo.svg"
-                className="w-32"
-                alt="tailus logo"
-              /> */}
           </Link>
         </div>
 
@@ -82,31 +77,6 @@ export default function NavBar() {
               </svg>
               <span className="-mr-1 font-medium">Resume</span>
             </Link>
-          </li>
-          <li>
-            {/* <Link
-                to=""
-                className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group"
-                >
-                <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                >
-                <path
-                className="fill-current text-gray-300 group-hover:text-cyan-300"
-                fillRule="evenodd"
-                d="M2 6a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1H8a3 3 0 00-3 3v1.5a1.5 1.5 0 01-3 0V6z"
-                clipRule="evenodd"
-                />
-                <path
-                className="fill-current text-gray-600 group-hover:text-cyan-600"
-                d="M6 12a2 2 0 012-2h8a2 2 0 012 2v2a2 2 0 01-2 2H2h2a2 2 0 002-2v-2z"
-                />
-                </svg>
-                <span className="group-hover:text-gray-700">Categories</span>
-              </Link> */}
           </li>
           <li>
             <Link
@@ -201,26 +171,6 @@ export default function NavBar() {
             </Link>
           </li>
         </ul>
-      </div>
-
-      <div className="px-6 -mx-6 pt-4 flex justify-between items-center border-t">
-        <button className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
-            />
-          </svg>
-          <span className="group-hover:text-gray-700">Logout</span>
-        </button>
       </div>
     </aside>
   );
