7f7f3b35e8d3ae57d337c8bfb50c69902b982ea3
Grace Durant <gdurant34@gmail.com>
2023-05-09 10:57:28 -0700

    hamberger menu when page is smaller

--- apps/website/src/app/HomePage.tsx
+++ apps/website/src/app/HomePage.tsx
@@ -16,6 +16,12 @@ export function HomePage({ title }: { title: string }) {
                 viewBox="0 0 24 24"
                 stroke="currentColor"
               >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
               </svg>
             </button>
             <div className="flex space-x-4">
