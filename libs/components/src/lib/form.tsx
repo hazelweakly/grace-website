1eea203cbfbc5484e8c16bdfcb1f9c32e16fcbb2
Grace Durant <gdurant34@gmail.com>
2023-06-02 00:20:23 -0700

    added form component

---
+++ libs/components/src/lib/form.tsx
@@ -0,0 +1,14 @@
+import { Root, Field, Label, Message, Control, Submit } from '@radix-ui/react-form';
+
+export const Form = () => (
+    <Root>
+        <Field>
+            <Label></Label>
+            <Message></Message>
+            <Control></Control>
+        </Field>
+        <Submit></Submit>
+    </Root>
+);
+
+export default Form;
