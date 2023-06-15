ec621b02342f244cdd90fb0b233cc187154fecb7
Grace Durant <gdurant34@gmail.com>
2023-06-14 17:17:39 -0700

    removed duplicate page and renamed for contact

--- apps/website/src/app/AboutMe.tsx
+++
@@ -1,9 +0,0 @@
-const AboutMe = ({ title }: { title: string }) => {
-    return (
-        <div className="ml-auto mb-6 lg:w-[75%] xl:w-[80%] 2xl:w-[85%]">
-            <p>About Me</p>
-        </div>
-    )
-}
-
-export default AboutMe;
