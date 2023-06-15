a67b2a4fdf5a6fee373d58905a8dac9c7be46374
Grace Durant <gdurant34@gmail.com>
2023-06-14 19:02:49 -0700

    added links to contact page

--- apps/website/src/app/ContactPage.tsx
+++ apps/website/src/app/ContactPage.tsx
@@ -1,9 +1,58 @@
 const AboutMe = ({ title }: { title: string }) => {
-    return (
-        <div className="ml-auto mb-6 lg:w-[75%] xl:w-[80%] 2xl:w-[85%]">
-            <p>Contact</p>
+  return (
+    <div className="ml-auto mb-6 lg:w-[75%] xl:w-[80%] 2xl:w-[85%] bg-slate-50 h-screen">
+      <img
+        src="/assets/images/annie-spratt-PM4Vu1B0gxk-unsplash.jpg"
+        alt=""
+        className="w-full h-full object-cover absolute mix-blend-overlay opacity-90"
+      />
+      <div className="relative overflow-hidden rounded-lg bg-cover bg-no-repeat p-12 text-center">
+        <div className="flex h-full items-center justify-center">
+          <div className="text-slate-600">
+            <h1 className="mb-4 text-5xl font-semibold text-black-900">
+              {' '}
+              Contact
+            </h1>
+            <p className="mb-6">
+              <a
+                href="https://github.com/gdurant34"
+                className="text-teal-700 mb-6 text-xl font-semibold"
+              >
+                {' '}
+                Github
+              </a>
+            </p>
+            <p className="mb-6">
+              <a
+                href="https://www.linkedin.com/in/gracedurant/"
+                className="text-teal-700 mb-6 text-xl font-semibold"
+              >
+                {' '}
+                LinkedIn
+              </a>
+            </p>
+            <p className="mb-6">
+              <a
+                href="https://medium.com/@gdurant34"
+                className="text-teal-700 mb-6 text-xl font-semibold"
+              >
+                {' '}
+                Medium
+              </a>
+            </p>
+            <button
+              type="button"
+              className="rounded border-2 border-slate-700 px-7 pb-[8px] pt-[10px] text-sm font-medium uppercase leading-normal text-slate-700 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-teal-500 hover:bg-opacity-10 hover:text-green-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
+              data-te-ripple-init
+              data-te-ripple-color="light"
+            >
+              Email me
+            </button>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default AboutMe;
